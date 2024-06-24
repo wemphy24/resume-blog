2de86f29334f3bfa67dbf3da48e58c8a426265b1
wemphy <wemphypool@gmail.com>
2024-06-24 22:29:35 +0700

    Minor Update

--- pages/about.vue
+++ pages/about.vue
@@ -7,17 +7,17 @@
       <div class="text-2xl font-medium">About Me</div>
       <div class="text-xl mt-2 text-justify">
         Hello, let me introduce myself. My name is Wemphy Stephian Philipe, and
-        I am a fresh graduate who just graduated in August 2023. Currently, I
-        work as a freelance UI/UX designer and web developer on an online work
-        platform. I have a keen interest and skills in web development, UI/UX
-        design, and data analysis to address business process needs in solving
-        problems. I am proficient in using the TALL stack (Tailwind, Alpine,
-        Laravel, Livewire) and the MEVN stack (MongoDB, Express.js, Vue.js,
-        Node.js) to build dynamic, responsive, and efficient web applications.
-        I'm also studying other exciting technologies like React and Flutter.
-        I've always tried to expand my knowledge and keep learning and
-        developing to be at the forefront of the ever-evolving technology
-        landscape.
+        I am a fresh graduate with bachelor degree in Computer Science.
+        Currently, I work as a freelance UI/UX designer and web developer on an
+        online work platform. I have a keen interest and skills in web
+        development, UI/UX design, and data analysis to address business process
+        needs in solving problems. I am proficient in using the TALL stack
+        (Tailwind, Alpine, Laravel, Livewire) and the MEVN stack (MongoDB,
+        Express.js, Vue.js, Node.js) to build dynamic, responsive, and efficient
+        web applications. I'm also studying other exciting technologies like
+        React and Flutter. I've always tried to expand my knowledge and keep
+        learning and developing to be at the forefront of the ever-evolving
+        technology landscape.
       </div>
       <div class="py-10 text-center">
         <div
