67137feeaef676de11690a4a891963dd1ca587be
Wemphy Stephian <wemphysp@gmail.com>
2026-01-05 11:13:13 +0800

    update bug project

--- components/CardProject.vue
+++ components/CardProject.vue
@@ -1,16 +1,13 @@
 <template>
   <NuxtLink
     :to="linkProject"
-    class="bg-white rounded-xl hover:scale-105 hover:-translate-x-0 hover:duration-500 w-full"
+    class="bg-white rounded-xl w-full"
   >
-
   <div class="flex flex-col gap-4 pb-4">
     <img class="rounded-t-xl w-full" :src="imgPreview" alt={{ title }} /> 
-  
     <div class="text-center text-xl font-medium hover:scale-110 hover:duration-500">
       {{ title }}
     </div>
-
     <div class="flex items-center justify-center gap-4">
       <div
         class="link bg-[#18181B] rounded-xl w-fit px-4 py-2 text-sm text-center text-white"
